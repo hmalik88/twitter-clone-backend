dc67d7d6a2635ac9c57b179991cffd96eb558fb5
hmalik88 <hbmalik88@gmail.com>
2020-11-03 14:00:22 -0500

    fleshed out the server file, need to return to it and add model synchronization.

---
+++ server.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const app = require('./app');
+const port = '3000';
+app.set('port', port);
+const server = http.createServer(app);
+
+// sync models here
+
+
+// event listener for HTTP server error events
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    //handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    console.log('Listening on ' + bind);
+}
