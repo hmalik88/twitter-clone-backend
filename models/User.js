24acbe6d5e70ec176100c866555a42714b273d84
hmalik88 <hbmalik88@gmail.com>
2020-11-03 14:21:28 -0500

    defined User model.

---
+++ models/User.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    let User = sequelize.define('User', {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true
+        },
+        userName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        ethAddress: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            unique: true
+        }
+    })
+    return User;
+}
