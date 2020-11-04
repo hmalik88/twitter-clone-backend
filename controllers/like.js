d778d1ce8a8ba7fc937b09a1c176543f741fb7ab
hmalik88 <hbmalik88@gmail.com>
2020-11-04 10:20:29 -0500

    wrote the create/delete like handlers.

--- controllers/like.js
+++ controllers/like.js
@@ -1,9 +1,42 @@
 const Like = require('../models/Like');
+const { validateToken } = require('../validators/userValidations');
+const { validateLiking } = require('../validators/likeValidations');
+const { isEmpty } = require('lodash');
+const jwt = require('jsonwebtoken');
+
+
 
 exports.create_like = async function(req, res, next) {
+    let errors = await validateToken({}, req);
+    if (!isEmpty(errors)) next(errors);
+    errors = await validateLiking(errors, req);
+    if (!isEmpty(errors)) next(errors);
+    else {
+        const token = req.headers('x-authentication-token');
+        const userId = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET).id;
+        const newLike = await Like.create({
+            user_id: userId,
+            tweet_id: req.body.tweetId
+        });
+        res.status(200).send({like: newLike});
+    }
 
 }
 
 exports.remove_like = async function(req, res, next) {
-    
+    let errors = await validateToken({}, req);
+    if (!isEmpty(errors)) next(errors);
+    errors = await validateLiking(errors, req);
+    if (!isEmpty(errors)) next(errors);
+    else {
+        const token = req.headers('x-authentication-token');
+        const userId = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET).id;
+        const deletedLike = await Like.destroy({
+            where: {
+                user_id: userId,
+                tweet_id: req.body.tweetId
+            }
+        });
+        res.status(200).send({deletedlike: deletedLike});
+    }
 }
