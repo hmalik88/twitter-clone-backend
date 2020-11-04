4c904cf2aae842bfc71c8df6651f6031da3bd1ce
hmalik88 <hbmalik88@gmail.com>
2020-11-03 21:20:14 -0500

    fleshed out signup and login handlers.

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { validateSignup, validateLogin, validateToken } = require('../validators/userValidations');
 const { isEmpty } = require('lodash');
 const User = require('../models/User');
 
@@ -8,9 +9,29 @@ const generateHash = function(password) {
 }
 
 exports.signup = async function(req, res, next) {
+    const errors = await validateSignup({}, req);
+    if (!isEmpty(errors)) next(errors);
+    else {
+        const newUser = await User.create({
+            email: req.body.email,
+            password: generateHash(req.body.password),
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            ethAddress: req.body.ethAddress,
+            userName: req.body.userName
 
+        });
+        const jwtToken = jwt.sign({id: newUser.id}, process.env.ACCESS_TOKEN_SECRET);
+        res.status(200).send({ token: jwtToken });
+    }
 }
 
 exports.login = async function(req, res, next) {
-
+    const errors = await validateLogin({}, req);
+    if (!isEmpty(errors)) next(errors);
+    else {
+        const user = await models.User.findOne({where: {email: req.body.email}});
+        const jwtToken = jwt.sign({id: user.id}, process.env.ACCESS_TOKEN_SECRET);
+        res.status(200).send({ token: jwtToken });
+    }
 }
