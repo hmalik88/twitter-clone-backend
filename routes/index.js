f232bdb18d4e0bff3281aa7817cef5c448e1b471
hmalik88 <hbmalik88@gmail.com>
2020-11-04 13:08:09 -0500

    plugged in controllers to the routes.

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,32 @@
 const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
+const user = require('../controllers/user');
+const tweet = require('../controllers/tweet');
+const like = require('../controllers/like');
+const follower = require('../controllers/follower');
 
 // user routes
 
-router.post('/signup');
-router.post('/login');
+router.post('/signup', user.signup);
+router.post('/login', user.login);
 
 
 // follow routes
 
-router.post('/follow/:id');
-router.delete('/follow/:id');
+router.post('/follow/:id', follower.create_following);
+router.delete('/follow/:id', follower.delete_following);
 
 
 //  tweet routes
 
-router.post('/tweets');
-router.get('/users/:id/timeline');
-router.get('/users/:id/tweeths');
+router.post('/tweets', tweet.create_tweet);
+router.get('/users/:id/timeline', tweet.show_timeline);
+router.get('/users/:id/tweeths', tweet.show_tweeths);
 
 // like routes
 
-router.post('/likes');
-router.delete('/likes/:id');
+router.post('/likes', like.create_like);
+router.delete('/likes/:id', like.remove_like);
 
 module.exports = router;
