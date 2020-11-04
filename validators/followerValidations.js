1ae0de5860c6778bbf3c658aee7bc959ad270e13
hmalik88 <hbmalik88@gmail.com>
2020-11-04 09:23:32 -0500

    fixed typo of req.body.user_id to req.params.id.

--- validators/followerValidations.js
+++ validators/followerValidations.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken');
 // make sure the follower_id matches up with the id in the JWT
 
 exports.validateFollowing = async function(errors, req) {
-    if (req.body.user_id == null || req.body.follower_id == null) {
+    if (req.params.id == null || req.body.follower_id == null) {
         errors["status"] = 400;
         errors["message"] = "user_id and follower_id fields must both be present"
         return errors;
@@ -19,7 +19,7 @@ exports.validateFollowing = async function(errors, req) {
         errors["message"] = "You do not have access to this resource."
         return errors;
     }
-    const users = await User.findAll({where: {id: [user_id, follower_id]}});
+    const users = await User.findAll({where: {id: [userId, req.params.id]}});
     if (users == null || users.length !== 2) {
         errors["status"] = 400;
         errors["message"] = "Both follower and followee must be valid users."
