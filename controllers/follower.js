cbb42cdc9218e6b7dfffd89e5cf32eb3ed6827a2
hmalik88 <hbmalik88@gmail.com>
2020-11-04 09:10:04 -0500

    added in handlers for creating and deleting a following.

--- controllers/follower.js
+++ controllers/follower.js
@@ -1,9 +1,34 @@
 const Follower = require('../models/Follower');
+const { validateFollowing } = require('../validators/followerValidations');
+const { validateToken } = require('../validators/userValidations');
+const { isEmpty } = require('lodash');
+const jwt = require('jsonwebtoken');
+
+// validate the token first then validate that there's a user_id and follower_id
 
 exports.create_following = async function(req, res, next) {
-
+    let errors = await validateToken({}, req);
+    if (!isEmpty(errors)) next(errors); 
+    errors = await validateFollowing(errors, req);
+    if (!isEmpty(errors)) next(errors);
+    else {
+        const following = await Follower.create({user_id: req.body.user_id, follower_id: req.body.follower_id});
+        res.status(200).send({following: following});
+    }
 }
 
 exports.delete_following = async function(req, res, next) {
-    
+    let errors = await validateToken({}, req);
+    if (!isEmpty(errors)) next(errors);
+    errors = await validateFollowing(errors, req);
+    if (!isEmpty(errors)) next(errors);
+    else {
+        const deletedFollowing = await Follower.destroy({
+            where: {
+                user_id: req.body.user_id,
+                follower_id: req.body.follower_id
+            }
+        });
+        res.status(200).send({deleted_following: deletedFollowing});
+    }
 }
